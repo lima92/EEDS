20eeb51b831acd44495846395925c3938059746f
Kjetil Aune <aune.kjetil@gmail.com>
2015-02-25 21:30:40 +0100

    lol

--- Ex2/songs.h
+++ Ex2/songs.h
@@ -3,6 +3,7 @@
 typedef struct Song{
 	uint8_t length;
 	uint8_t note[];
+	uint8_t bpm;
 } Song;
 
-Song LISA = {TODO, {G4, A5, B5, C5, D5, D5, D5, D5, E5, E5, E5, E5, F5, F5, F5, F5}}
+Song LISA = { 18, {1, G4, 1, A5, 1, B5, 1, C5, 2, D5, 2, D5, 4, E5, 4, F5, 4, E5}, 144 }
