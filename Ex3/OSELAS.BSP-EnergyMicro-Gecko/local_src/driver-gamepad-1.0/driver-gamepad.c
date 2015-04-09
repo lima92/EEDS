8014f2068fd210b8cc52a2b74f367ea9bb80cf19
Kjetil Aune <aune.kjetil@gmail.com>
2015-04-09 15:21:14 +0200

    Add request for mem_region

--- Ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ Ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -5,6 +5,8 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/ioport.h>
+#include <efm32gg.h>
 
 /*
  * template_init - function to insert this module into kernel space
@@ -15,9 +17,18 @@
  * Returns 0 if successfull, otherwise -1
  */
 
+#define uint_32t *err;
+
 static int __init template_init(void)
 {
 	printk("Hello World, here is your module speaking\n");
+	*err = request_mem_region(GPIO_PC_BASE + GPIO_PC_DIN, 32, "GPIO IN");
+	if (*err == NULL){
+		printk("Failure");
+	}
+	else{
+		printk("GPIO INPUT SUCCESS");
+	}
 	return 0;
 }
 
@@ -30,7 +41,8 @@ static int __init template_init(void)
 
 static void __exit template_cleanup(void)
 {
-	 printk("Short life for a small module...\n");
+	release_mem_region(GPIO_PC_BASE + GPIO_PC_DIN, 32);
+	printk("Short life for a small module...\n");
 }
 
 module_init(template_init);
