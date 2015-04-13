6de013f95cb9af802b3661b82f579d37d7106f4e
Kjetil Aune <aune.kjetil@gmail.com>
2015-04-13 22:06:39 +0200

    setipGPIO mby

--- Ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ Ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -27,12 +27,17 @@ static int __init template_init(void)
 	printk("Hello World, here is your module fucking\n");
 	*err = request_mem_region(GPIO_PC_DIN, 32, "GPIO IN");
 	*ioremap = ioremap_nocache(GPIO_PC_DIN, 32);
+
 	if (*err == NULL){
 		printk("Failure\n");
+		return -1;
 	}
 	else{
 		printk("GPIO INPUT SUCCESS\n");
 	}
+
+	setup_GPIO();
+	
 	return 0;
 }
 
