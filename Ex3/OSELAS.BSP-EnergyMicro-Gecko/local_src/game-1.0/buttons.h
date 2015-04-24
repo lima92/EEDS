d2ff26a257d9de6e698fd40730e2983aec5f3591
Kjetil Aune <aune.kjetil@gmail.com>
2015-04-24 09:40:38 +0200

    Add button macro

---
+++ Ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/buttons.h
@@ -0,0 +1,9 @@
+
+#define sw1 0b00000001
+#define sw2 0b00000010
+#define sw3 0b00000100
+#define sw4 0b00001000
+#define sw5 0b00010000
+#define sw6 0b00100000
+#define sw7 0b01000000
+#define sw8 0b10000000
